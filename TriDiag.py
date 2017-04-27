b2c0331a31f72312c91c9fd8c5d8f287ff8ae8f4
Louis-Francois Arsenault <lf.arsenault@gmail.com>
2017-04-27 02:40:48 -0400

    Add files via upload

---
+++ TriDiag.py
@@ -0,0 +1,42 @@
+import numpy as np
+from numpy.ctypeslib import ndpointer
+import ctypes
+
+
+class TriDiagSol:
+#############################################################################################
+#			Thomas algorithm for Tridiagonal matrix see: 			    #
+#		  https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm		    #
+#											    #
+#                       Stable for symmetric positive definite matrices			    #
+#       Louis-Francois Arsenault, Columbia Universisty (2013-2017), la2518@columbia.edu     #
+#############################################################################################
+#                                                                                           #
+#       The class definition enables to load the compiled solver at once, hence why a class #
+#                                                                                           #
+#       FUNCTION solve:                                                                     #
+#               ld          : Lower diagonal vector					    #
+#               d           : Principal diagonal vector					    #
+#		ud	    : Upper diagonal vector					    #
+#		b	    : Vector of rhs of the system Ax=b where A tridiagonal matrix   # 
+#			      that would be formed with ld, d and ud   			    #
+#											    #
+#	OUTPUT:										    #
+#		x	    : Solution vector of Ax=b					    #
+#											    #
+############################################################################################# 
+
+	def __init__(self):
+
+		_lib = ctypes.cdll.LoadLibrary("./TriDiagSolver.so")
+		_func=_lib.TriDiagSolver
+		_func.restype=None
+		_func.argtypes=[ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),ctypes.c_int]
+		self._func=_func
+
+	def solve(self,ld,d,ud,b):
+
+		n=len(d)
+		x=np.zeros(n)
+		self._func(d,ld,ud.copy(),b.copy(),x,n)
+		return x
---
+++ TriDiagSolver.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+//		Thomas algorithm for Tridiagonal matrix see: https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
+//					Stable for symmetric positive definite matrices
+
+
+//					To compile as a shared library use the command:
+
+//				     gcc -shared -fPIC TriDiagSolver.c -o TriDiagSolver.so
+
+//				  ****Could probably be optimized a bit in the forward loop****
+
+
+void TriDiagSolver(double *d,double *ld,double *ud,double *b,double *res,int size_d)
+{
+	double den;
+	double num;
+
+	int i,j;
+
+	int size_ld,size_ud;
+	size_ld=size_d-1;
+	size_ud=size_ld;
+
+	//Forward
+	ud[0]=ud[0]/d[0];
+	b[0]=b[0]/d[0];
+	for(i=1; i<size_ud; i++)
+	{
+		den=d[i]-ld[i-1]*ud[i-1];
+		num=b[i]-ld[i-1]*b[i-1];
+		ud[i]/=den;
+		b[i]=num/den;
+	}
+
+	den=d[size_d-1]-ld[size_ld-1]*ud[size_ud-1];
+        num=b[size_d-1]-ld[size_ld-1]*b[size_d-2];
+	b[size_d-1]=num/den;
+
+	//Backward
+	res[size_d-1]=b[size_d-1];
+	for(j=size_d-2; j>=0 ; j--)
+		res[j]=b[j]-ud[j]*res[j+1];
+}
